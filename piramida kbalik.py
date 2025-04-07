73b4bd5e0dcab14f3438a9811ab60474fd2b8379
ElTheVain <gloryvain39@gmail.com>
2025-04-07 20:24:54 +0700

    Update piramida kbalik.py

--- piramida kbalik.py
+++ piramida kbalik.py
@@ -8,3 +8,15 @@ def print_star_pattern(n):
 # Jumlah baris pola
 n = 7
 print_star_pattern(n)
+
+#berlawanan arah 
+def print_star_pattern(n):
+    for i in range(n):
+        # Mencetak spasi
+        print(' ' * (2 * (n - i - 1)), end='')
+        # Mencetak bintang
+        print('* ' * (i + 1))
+
+# Jumlah baris pola
+n = 6
+print_star_pattern(n)
